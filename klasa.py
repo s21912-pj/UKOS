008fff8fe07078c3faa8b4ee63cf5677591ac090
Karol Kuchnio <s21912@pjwstk.edu.pl>
2019-12-14 13:47:25 +0100

    diff example

--- klasa.py
+++ klasa.py
@@ -6,5 +6,8 @@ class Klasa:
     def string(self,s):
         return self.x.join(s)
 
-k = Klasa('x')
-print(k.string(sys.argv))
+class Inna:
+    def __str__(self):
+        return 'Siema'
+i = Inna();
+print(i)
